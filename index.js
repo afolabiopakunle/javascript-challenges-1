80b9879e3471b44947f9359c27ae781dfaeca555
afolabiopakunle <opakunleafolabi@gmail.com>
2022-02-01 08:42:19 +0100

    reverse 1

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function reverseString1(str) {
+  let splited = str.split('');
+  splited.reverse();
+  let joined = splited.join("")
+  return joined
+}
+
+
+console.log(reverseString1('akindele'))
